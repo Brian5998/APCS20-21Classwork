d5a2c6a1fe936f36e971da6f9cb1aa8c05b64e4f
Brian Zhou <23zhou@st-23zhoubr.lan>
2020-09-03 10:38:09 -0400

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,12 @@
+public class MyFirstClass {
+
+        public static void main(String [] args) {
+            System.out.println("\"Dark theme is for the cool kids.\"");
+            String name = "Brian";
+            sayName(name);
+        }
+
+        public static void sayName(String name){
+            System.out.println("Said by " + name);
+        }
+    }
